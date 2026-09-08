e755ff7f710a64e05cd09a990fea4de22fd3a73d
Denchik <rock333shadow@gmail.com>
2026-09-08 17:43:06 +0300

    A draft of the main page interface has been created.

---
+++ lib/ui/home/home_drawer.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:sonora/app/app_router.dart';
+
+import 'package:sonora/core/l10n/l10n.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            title: Text(context.l10n.settings),
+            onTap: () {
+              Navigator.pop(context);
+              Navigation(context).pushRoute(AppRoutes.settings);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:sonora/app/app_router.dart';
-
-import 'package:sonora/core/l10n/l10n.dart';
 import 'package:sonora/core/theme/theme.dart';
+
+import 'home_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,9 +13,7 @@ class HomePage extends StatelessWidget {
       backgroundColor: context.colors.surface,
 
       appBar: AppBar(
-        title: Text(
-          'Sonora',
-        ),
+        title: Text('Sonora'),
 
         leading: Builder(
           builder: (context) {
@@ -25,37 +22,88 @@ class HomePage extends StatelessWidget {
                 Scaffold.of(context).openDrawer();
               },
 
-              icon: Icon(
-                Icons.menu
-              ),
+              icon: Icon(Icons.menu),
             );
           }
         ),
       ),
 
-      body: Center(
-        child: Card(
-          color: context.colors.primary,
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(context.l10n.denchik),
-          ),
-        ),
-      ),
-
-      drawer: Drawer(
-        child: ListView(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        child: Column(
           children: [
-            ListTile(
-              title: Text(context.l10n.settings),
-              onTap: () {
-                Navigator.pop(context);
-                Navigation(context).pushRoute(AppRoutes.settings);
-              },
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 190,
+                  height: 320,
+                  decoration: BoxDecoration(
+                    color: context.colors.primary,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+
+                  child: const Center(child: Text('Player')),
+                ),
+
+                const Spacer(),
+
+                Column(
+                  children: [
+                    Container(
+                      width: 160,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: context.colors.primary,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+
+                      child: const Center(child: Text('Your artists')),
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    Container(
+                      width: 160,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: context.colors.primary,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+
+                      child: const Center(child: Text('Your playlists')),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 22),
+
+            SizedBox(
+              height: 90,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 90,
+                    margin: const EdgeInsets.only(right: 8),
+                    decoration: BoxDecoration(
+                      color: context.colors.primary,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+
+                    child: Center(child: Text('Your song feed')),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
+
+      drawer: HomeDrawer(),
     );
   }
 }
