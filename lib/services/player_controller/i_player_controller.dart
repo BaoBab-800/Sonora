d74adbf728ef9ba68c3f076ca5c35da9c22452bf
Denchik <rock333shadow@gmail.com>
2026-09-10 17:58:31 +0300

    An abstraction for the player controller has been prepared.

---
+++ lib/services/player_controller/i_player_controller.dart
@@ -0,0 +1,26 @@
+import 'package:sonora/data/player_controller/controller_state.dart';
+import 'package:sonora/data/player_controller/track.dart';
+import 'package:sonora/data/player_controller/repeat_mode.dart';
+
+abstract interface class IPlayerController {
+  Stream<ControllerState> get stateStream;
+  ControllerState get state;
+
+  // Делегируется в PlaybackEngine
+  Future<void> play();
+  Future<void> pause();
+  Future<void> seek(Duration position);
+  Future<void> setVolume(double volume);
+
+  // Управление очередью
+  Future<void> setQueue(List<Track> tracks, {int startIndex = 0});
+  Future<void> addToQueue(Track track);
+  Future<void> removeFromQueue(String trackId);
+  Future<void> next();
+  Future<void> previous();
+  Future<void> skipTo(int index);
+  void setRepeatMode(RepeatMode mode);
+  void setShuffle(bool enabled);
+
+  Future<void> dispose();
+}
