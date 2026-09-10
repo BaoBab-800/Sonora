fea88ea36897b17997752cb4d482e0327e4773ac
Denchik <rock333shadow@gmail.com>
2026-09-10 18:12:51 +0300

    The PlaybackQueue model has been adjusted to allow reverting to the original queue.

--- lib/data/player_controller/playback_queue.dart
+++ lib/data/player_controller/playback_queue.dart
@@ -2,22 +2,21 @@ import 'package:flutter/cupertino.dart' as cupertino;
 
 import 'track.dart';
 import 'repeat_mode.dart';
-import '../player_engine/warped.dart';
 
 @cupertino.immutable
 class PlaybackQueue {
   final List<Track> tracks;
+  final List<String> originalOrder;
   final int currentIndex;
   final RepeatMode repeatMode;
   final bool shuffleEnabled;
-  final List<int>? shuffleOrder;
 
   const PlaybackQueue({
     this.tracks = const [],
+    this.originalOrder = const [],
     this.currentIndex = -1,
     this.repeatMode = RepeatMode.none,
     this.shuffleEnabled = false,
-    this.shuffleOrder,
   });
 
   Track? get currentTrack => (currentIndex >= 0 && currentIndex < tracks.length) ? tracks[currentIndex] : null;
@@ -26,17 +25,17 @@ class PlaybackQueue {
 
   PlaybackQueue copyWith({
     List<Track>? tracks,
+    List<String>? originalOrder,
     int? currentIndex,
     RepeatMode? repeatMode,
     bool? shuffleEnabled,
-    Wrapped<List<int>?>? shuffleOrder,
   }) {
     return PlaybackQueue(
       tracks: tracks ?? this.tracks,
+      originalOrder: originalOrder ?? this.originalOrder,
       currentIndex: currentIndex ?? this.currentIndex,
       repeatMode: repeatMode ?? this.repeatMode,
       shuffleEnabled: shuffleEnabled ?? this.shuffleEnabled,
-      shuffleOrder: shuffleOrder != null ? shuffleOrder.value : this.shuffleOrder,
     );
   }
 }
