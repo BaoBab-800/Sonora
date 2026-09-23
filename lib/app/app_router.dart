df23ba5a823d20171bdce4a07a829de52aa1ac19
Denchik <rock333shadow@gmail.com>
2026-09-23 20:51:59 +0300

    The M-C-E testing page has been removed as it is no longer needed.

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../ui/home/home_page.dart';
-import '../ui/home/test_player.dart';
 import '../ui/settings/settings_page.dart';
 
 enum AppRoutes {
   home(name: 'home', path: '/'),
-  settings(name: 'settings', path: '/settings'),
-  testPlayer(name: 'testPlayer', path: '/test-player');
+  settings(name: 'settings', path: '/settings');
 
   final String name;
   final String path;
@@ -33,12 +31,6 @@ class AppRouter {
         name: AppRoutes.settings.name,
         path: AppRoutes.settings.path,
         builder: (context, state) => const SettingsPage(),
-      ),
-
-      GoRoute(
-        name: AppRoutes.testPlayer.name,
-        path: AppRoutes.testPlayer.path,
-        builder: (context, state) => const PlayerTest(),
       ),
     ],
   );
--- lib/ui/home/home_drawer.dart
+++ lib/ui/home/home_drawer.dart
@@ -19,14 +19,6 @@ class HomeDrawer extends StatelessWidget {
               Navigation(context).pushRoute(AppRoutes.settings);
             },
           ),
-
-          ListTile(
-            title: Text('Test player'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigation(context).pushRoute(AppRoutes.testPlayer);
-            },
-          ),
         ],
       ),
     );
